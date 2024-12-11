67a2731bf817faa137ba0258069da24ae5c00bd4
kwy97 <kwy1547@gmail.com>
2024-12-11 20:45:09 +0900

    241211

--- practice.py
+++ practice.py
@@ -1,8 +1,11 @@
-my_str = input()
-alphabet_occurrence_array = [-1] * 26
+def find_max_plus_or_multiply(array):
 
-for i in range(len(my_str)):
-    if alphabet_occurrence_array[ord(my_str[i]) - ord('a')] == -1:
-        alphabet_occurrence_array[ord(my_str[i]) - ord('a')] = i
 
-print(*alphabet_occurrence_array)
+
+    pass
+
+
+result = find_max_plus_or_multiply
+print("정답 = 728 현재 풀이 값 =", result([0,3,5,6,1,2,4]))
+print("정답 = 8820 현재 풀이 값 =", result([3,2,1,5,9,7,4]))
+print("정답 = 270 현재 풀이 값 =", result([1,1,1,3,3,2,5]))
