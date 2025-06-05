12a7900677b3b073a942329b513df2708a725986
kwy97 <kwy1547@gmail.com>
2025-06-05 21:46:16 +0900

    :memo: [docs] 3주차 마무리

---
+++ week_03/03_10_dict_chaining.py
@@ -0,0 +1,34 @@
+class LinkedTuple:
+    def __init__(self):
+        self.items = []
+
+    def add(self, key, value):
+        self.items.append((key, value))
+
+    def get(self, key):
+        for k, v in self.items:
+            if k == key:
+                return v
+
+class Linked_Dict:
+    def __init__(self):
+        self.items = []
+        for i in range(8):
+            self.items.append(LinkedTuple())
+
+    def put(self, key, value):
+        index = hash(key) % len(self.items)
+        self.items[index].add(key, value)
+
+    def get(self, key):
+        index = hash(key) % len(self.items)
+        return self.items[index].get(key)
+
+
+my_dict = Linked_Dict()
+my_dict.put("test", 3)
+my_dict.put("333", 7)
+my_dict.put("77", 6)
+
+print(my_dict.get("333"))
+print(my_dict.get("77"))
