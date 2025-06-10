14eaf96973e51753d42e5d0a26549a73f5c8ec0e
kwy97 <kwy1547@gmail.com>
2025-06-10 21:26:32 +0900

    :memo: [docs] 4주차 마무리

--- practice.py
+++ practice.py
@@ -1,40 +1,38 @@
-def get_melon_best_album(genre_array, play_array):
-    n = len(genre_array)
+def get_lines(n, w):
+    lines = -(-n // w)
+    return lines
 
-    genre_total_play_dict = {}
-    genre_index_play_dict = {}
+def solution(n, w, num):
+    lines = get_lines(n, w)
+    answer = 1
+    boxes = []
+    cur = 1
 
-    # 1. genre 별 총 play 횟수 뽑기
-    # 3. genre 내 곡 별 play 횟수 뽑기
-    for i in range(n):
-        genre = genre_array[i]
-        play = play_array[i]
-
-        if genre not in genre_total_play_dict:
-            genre_total_play_dict[genre] = play
-            genre_index_play_dict[genre] = [[i, play]]
+    for i in range(lines):
+        if i % 2 == 0:
+            # 정순으로
+            for j in range(cur, cur + w):
+                boxes.append(j)
+            cur += w
         else:
-            genre_total_play_dict[genre] += play
-            genre_index_play_dict[genre].append([i, play])
+            # 역순으로
+            for j in range(cur + w -1, cur - 1, -1):
+                boxes.append(j)
+            cur += w
 
-    # 2. 총 횟수가 높은 장르부터 앞에 오게 정렬
-    sorted_genre_total_play_dict = sorted(genre_total_play_dict.items(), key=lambda item: item[1], reverse=True)
+    for temp_num in range(cur-1, n, -1):
+        boxes[boxes.index(temp_num)] = 0
 
-    result = []
+    num_index = boxes.index(num)
+    next_index = num_index + w
 
-    for genre, total_play in sorted_genre_total_play_dict:
-        sorted_genre_index_play_array = sorted(genre_index_play_dict[genre], key=lambda item: item[1], reverse=True)
+    while next_index < cur-1:
+        if boxes[next_index] == 0:
+            break
 
-        genre_song_count = 0
-        for index, play in sorted_genre_index_play_array:
-            if genre_song_count == 2:
-                break
+        answer += 1
+        next_index += w
 
-            result.append(index)
-            genre_song_count += 1
+    return answer
 
-    return result
-
-
-print("정답 = [4, 1, 3, 0] / 현재 풀이 값 = ", get_melon_best_album(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
-print("정답 = [0, 6, 5, 2, 4, 1] / 현재 풀이 값 = ", get_melon_best_album(["hiphop", "classic", "pop", "classic", "classic", "pop", "hiphop"], [2000, 500, 600, 150, 800, 2500, 2000]))
+print(solution(6, 5, 4))
