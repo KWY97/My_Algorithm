708d0238a3794d621769b2e9ab4d589d69d50059
kwy97 <kwy1547@gmail.com>
2025-07-17 16:51:44 +0900

    :wrench: [chore] 파일구조 변경 (강의 추가)

--- practice.py
+++ practice.py
@@ -1,11 +1,23 @@
-s, k, h = map(int, input().split())
+def gcd(n1, n2):
+    temp = n1 % n2
 
-if s + h + k >= 100:
-    print("OK")
-else:
-    if (s < h < k) or (s < k < h):
-        print("Soongsil")
-    if (h < s < k) or (h < k < s):
-        print("Hanyang")
-    if (k < s < h) or (k < h < s):
-        print("Korea")
+    while temp != 0:
+        n1 = n2
+        n2 = temp
+        temp = n1 % n2
+
+    return n2
+
+
+c1, p1 = map(int, input().split())
+c2, p2 = map(int, input().split())
+
+cc = (c1 * p2) + (c2 * p1)
+cp = p1 * p2
+
+my_gcd = gcd(cc, cp)
+
+ans_c = cc // my_gcd
+ans_p = cp // my_gcd
+
+print(ans_c, ans_p)
