06555ac07e477710369f339a56f60d910bddfd6f
kwy97 <kwy1547@gmail.com>
2025-09-19 16:49:20 +0900

    :memo: [docs] 딕셔너리 문제 강의 풀이 추가 (리트코드)

--- inflearn/Namno/Dictionary/Longest_Consecutive_Sequence.py
+++ inflearn/Namno/Dictionary/Longest_Consecutive_Sequence.py
@@ -30,3 +30,26 @@ class Solution(object):
 
 
 # 강의 풀이
+# 내 풀이
+# class Solution(object):
+#     def longestConsecutive(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         longest = 0
+#         num_dict = {}
+#
+#         for num in nums:
+#             num_dict[num] = True
+#
+#         for num in num_dict:
+#             if num - 1 not in num_dict:
+#                 cnt = 1
+#                 target = num + 1
+#
+#                 while target in num_dict:
+#                     target += 1
+#                     cnt += 1
+#                 longest = max(longest, cnt)
+#         return longest
