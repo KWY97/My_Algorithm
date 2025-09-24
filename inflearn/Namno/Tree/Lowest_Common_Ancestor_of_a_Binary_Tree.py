45927d54fe4247cd6cea2d8c0d202279c0ece85b
kwy97 <kwy1547@gmail.com>
2025-09-24 21:41:49 +0900

    :memo: [docs] 트리 문제 (리트코드)

---
+++ inflearn/Namno/Tree/Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -0,0 +1,14 @@
+# 내 풀이
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root is None:
+            return None
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if root == p or root == q:
+            return root
+        elif left and right:
+            return root
+        return left or right
