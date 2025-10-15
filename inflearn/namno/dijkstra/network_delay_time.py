edf399419f527d5eddf1e10b3b55f39621ffe2ad
kwy97 <kwy1547@gmail.com>
2025-10-15 15:33:08 +0900

    :memo: [docs] 다익스트라 문제 (리트코드)

--- inflearn/namno/dijkstra/network_delay_time.py
+++ inflearn/namno/dijkstra/network_delay_time.py
@@ -1,6 +1,6 @@
 import heapq
 from collections import defaultdict
-
+# 기본 템플릿 방식
 class Solution(object):
     def networkDelayTime(self, times, n, k):
         graph = defaultdict(list)
@@ -23,3 +23,28 @@ class Solution(object):
         if len(costs) < n:
             return -1
         return max(costs.values())
+
+# Dist 기반 방식
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
+        graph = defaultdict(list)
+        for time in times:
+            graph[time[0]].append((time[2], time[1]))
+
+        dist = [10**15] * (n + 1)
+        dist[k] = 0
+        pq = []
+        heapq.heappush(pq, (0, k))
+
+        while pq:
+            cur_w, cur_v = heapq.heappop(pq)
+            if cur_w > dist[cur_v]:
+                continue
+            for w, next_v in graph[cur_v]:
+                next_w = cur_w + w
+                if next_w < dist[next_v]:
+                    dist[next_v] = next_w
+                    heapq.heappush(pq, (next_w, next_v))
+
+        ans = max(dist[1:])
+        return -1 if ans == 10**15 else ans
